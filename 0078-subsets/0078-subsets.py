be8477acadea9edfed7f775478b7b928ddcd80ef
Nely Viradiya <nely08ppsv20231@gmail.com>
2026-09-14 15:13:31 +0530

    Time: 0 ms (100%), Space: 12.5 MB (58.59%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums):
+        result = []
+        current = []
+
+        def backtrack(start):
+            result.append(current[:])
+
+            for i in range(start, len(nums)):
+                current.append(nums[i])
+                backtrack(i + 1)
+                current.pop()
+
+        backtrack(0)
+
+        return result
